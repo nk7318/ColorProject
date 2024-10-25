bc8d6ade7db9d6d6300ed289aa1745c76d218e32
nk7318 <nkyadav7318@gmail.com>
2024-10-25 17:57:49 +0530

    up

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div className={` w-screen h-screen ${color} flex flex-col justify-end p-5  items-center`}>
-    <img className=' w-full h-full lg:w-[700px] lg:h-[700px] ' src='https://png.pngtree.com/png-clipart/20230913/original/pngtree-programmer-clipart-developer-sitting-behind-his-computer-in-glasses-cartoon-vector-png-image_11075227.png'/>
+    <img className=' w-full h-full  xl:w-[700px] xl:h-[700px] ' src='https://png.pngtree.com/png-clipart/20230913/original/pngtree-programmer-clipart-developer-sitting-behind-his-computer-in-glasses-cartoon-vector-png-image_11075227.png'/>
     <div className=' lg:w-[60%] w-full flex  py-2 bg-white rounded-full justify-evenly text-white'>
     <button className='border border-slate-800 rounded-lg lg:rounded-full lg:px-3 px-1 py-2 bg-green-500' onClick={()=>setColor("bg-green-500")} >Green</button>
     <button  className='border border-slate-800 rounded-lg lg:rounded-full lg:px-3 px-1 py-2 bg-yellow-500' onClick={()=>setColor("bg-yellow-500")} >Yellow</button>
